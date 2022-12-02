a89199e1ad8452186c7b6a8e36bd07584937917c
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-12-02 12:20:36 +0100

    completed exercises 9.1-9.3

---
+++ First-Steps/bmiCalculator.ts
@@ -0,0 +1,61 @@
+import { question } from "readline-sync";
+
+interface rawValues {
+  rawHeight: string;
+  rawWeight: string;
+}
+
+interface parsedValues {
+  parsedHeight: number;
+  parsedWeight: number;
+}
+
+const getInput = (): rawValues => {
+  const rawHeight = question("Enter your height in cm: ");
+  const rawWeight = question("Enter your weight in kg: ");
+
+  return { rawHeight, rawWeight };
+};
+
+const parseInput = (rawHeight: string, rawWeight: string): parsedValues => {
+  if (Number(rawHeight) <= 0 || Number(rawWeight) <= 0) {
+    throw new Error("Height and Weight must be a positive value!");
+  }
+  if (!isNaN(Number(rawHeight)) && !isNaN(Number(rawWeight))) {
+    return {
+      parsedHeight: Number(rawHeight),
+      parsedWeight: Number(rawWeight),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / (height / 100) ** 2;
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi < 30) {
+    return "Overweight";
+  } else return "Obese";
+};
+
+try {
+  const { rawHeight, rawWeight } = getInput();
+  const { parsedHeight, parsedWeight } = parseInput(rawHeight, rawWeight);
+  console.log(calculateBmi(parsedHeight, parsedWeight));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
+
+console.log(calculateBmi(180, 74));
+console.log(calculateBmi(178, 98));
+console.log(calculateBmi(165, 65));
+console.log(calculateBmi(185, 72));
+console.log(calculateBmi(155, 82));
---
+++ First-Steps/exerciseCalculator.ts
@@ -0,0 +1,114 @@
+import { question } from "readline-sync";
+
+interface rawValues {
+  rawTarget: string;
+  rawDailyHours: string[];
+}
+
+interface parsedValues {
+  parsedTarget: number;
+  parsedDailyHours: number[];
+}
+
+const getInput = (): rawValues => {
+  const rawTarget = question("What is your target value? ");
+
+  let dayNumber = 1;
+  const rawDailyHours: string[] = [];
+
+  while (true) {
+    const input = question(
+      `How many hours did you exercise on day ${dayNumber}? (Press 'enter' to quit): `
+    );
+
+    if (input) {
+      rawDailyHours.push(input);
+      dayNumber++;
+    } else {
+      break;
+    }
+  }
+
+  return { rawTarget, rawDailyHours };
+};
+
+const parseInput = (
+  rawTarget: string,
+  rawDailyHours: string[]
+): parsedValues => {
+  if (Number(rawTarget) <= 0) {
+    throw new Error("Target must be a positive value!");
+  }
+  if (rawDailyHours.length === 0) {
+    throw new Error("Provide at least one value for exercised days!");
+  }
+  if (
+    !isNaN(Number(rawTarget)) &&
+    !rawDailyHours.map((hour) => Number(hour)).some(isNaN)
+  ) {
+    return {
+      parsedTarget: Number(rawTarget),
+      parsedDailyHours: rawDailyHours.map((hour) => Number(hour)),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (
+  dailyExerciseHours: number[],
+  target: number
+): Result => {
+  const periodLength = dailyExerciseHours.length;
+  const trainingDays = dailyExerciseHours.filter((day) => day > 0).length;
+  const average = dailyExerciseHours.reduce((a, b) => a + b, 0) / periodLength;
+  const success = average >= target ? true : false;
+
+  let rating;
+  let ratingDescription;
+
+  if (target > average) {
+    rating = 1;
+    ratingDescription = "There is room for improvement, you can do it!";
+  } else if (target === average) {
+    rating = 2;
+    ratingDescription = "Good job!";
+  } else {
+    rating = 3;
+    ratingDescription = "Fantastic! Keep it going!";
+  }
+
+  return {
+    periodLength: periodLength,
+    trainingDays: trainingDays,
+    success: success,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average,
+  };
+};
+
+try {
+    const { rawTarget, rawDailyHours } = getInput();
+    const { parsedTarget, parsedDailyHours } = parseInput(
+      rawTarget,
+      rawDailyHours
+    );
+    console.log(calculateExercises(parsedTarget, parsedDailyHours));
+  } catch (error) {
+    if (error instanceof Error)
+      console.log("Error, something bad happened, message: ", error.message);
+  }
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
